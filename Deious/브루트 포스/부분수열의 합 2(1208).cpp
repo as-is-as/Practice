1d9abaf02a80930ec3b5b8927132ce6fa65e026a
deious <gkstjddlekd@gmail.com>
2020-11-30 22:01:35 +0900

    Update 부분수열의 합 2(1208).cpp

--- Deious/브루트 포스/부분수열의 합 2(1208).cpp
+++ Deious/브루트 포스/부분수열의 합 2(1208).cpp
@@ -1,45 +1,75 @@
 #include <cstdio>
 #include <vector>
+#include <bitset>
 #include <algorithm>
 
 using namespace std;
 
+int nums[40];
+
 int main()
 {
-	int n, s, answer = 0;
+	int n, s;
 	scanf("%d %d", &n, &s);
-
-	vector<int> v(n);
-
-	for (int i = 0; i < v.size(); i++)
-	{
-		scanf("%d", &v[i]);
-	}
-
-	int count = n - 1;
-
 	for (int i = 0; i < n; i++)
 	{
-		if (v[i] == s) answer++;
+		scanf("%d", &nums[i]);
 	}
 
-	while (count--)
+	vector<int> left(1 << n / 2), right(1 << n - n / 2);
+
+	for (int i = 0; i < 1 << n / 2; i++)
 	{
-		vector<int> v1(n);
-		for (int i = n - 1; i >= count; i--) v1[i] = 1;
-		do
+		bitset<20> bit(i);
+		for (int j = 0; j < n / 2; j++)
 		{
-			int num = 0;
-			for (int j = 0; j < v.size(); j++)
+			if (bit[j]) left[i] += nums[j];
+		}
+	}
+
+	for (int i = 0; i < 1 << n - n / 2; i++)
+	{
+		bitset<20> bit(i);
+		for (int j = 0; j < n - n / 2; j++)
+		{
+			if (bit[j]) right[i] += nums[j + n / 2];
+		}
+	}
+
+	sort(left.begin(), left.end());
+	sort(right.begin(), right.end());
+
+	int start = 0, end = (1 << n - n / 2) - 1;
+	long long ans = 0;
+
+	while (true)
+	{
+		if (start >= 1 << n / 2 || end < 0) break;
+
+		int next = left[start] + right[end];
+		if (next > s) end--;
+		else if (next < s) start++;
+		else
+		{
+			long long leftCount = 1, rightCount = 1;
+			while (start + 1 < 1 << n / 2 && left[start] == left[start + 1])
 			{
-				if (v1[j] == 1) num += v[j];
+				leftCount++;
+				start++;
+			}
+			while (end - 1 >= 0 && right[end] == right[end - 1])
+			{
+				rightCount++;
+				end--;
 			}
 
-			if (num == s) answer++;
-		} while (next_permutation(v1.begin(), v1.end()));
+			ans += leftCount * rightCount;
+			start++;
+			end--;
+		}
 	}
 
-	printf("%d\n", answer);
-
+	if (s == 0) ans--;
+	printf("%lld\n", ans);
 	return 0;
 }
